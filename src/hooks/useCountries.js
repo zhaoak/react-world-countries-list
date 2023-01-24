38bb8af21ee70620ccca8cc61a0a849227a23b4c
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-24 15:35:38 -0800

    added try catch block to useCountries hook

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -4,12 +4,17 @@ import { fetchCountries } from '../services/client.js';
 export default function useCountries() {
   // set up state variables
   const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // fetch from supabase, store data in state
     const fetchData = async () => {
-      const response = await fetchCountries();
-      setCountries(response);
+      try {
+        const response = await fetchCountries();
+        setCountries(response);
+      } catch (e) {
+        setError('Oh no, fetching the data failed!');
+      }
     };
     fetchData();
   }, []);
